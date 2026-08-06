656a1e0b21794230d57352abb734b3885a248f73
Harvesthub121 <harvesthubindustries@gmail.com>
2026-08-06 21:06:04 +0800

    Rename Vida Partners -> Karrkarr

--- apps/api/src/modules/jobs/processors/risk-scoring.processor.ts
+++ apps/api/src/modules/jobs/processors/risk-scoring.processor.ts
@@ -19,7 +19,7 @@ import { Job } from 'bullmq';
 import { PrismaService } from '../../../prisma/prisma.service';
 import { PolicyService } from '../../policy/policy.service';
 import { NotificationService } from '../../notifications/notification.service';
-import { POLICY_KEYS, daysOverdue } from '@vida/shared';
+import { POLICY_KEYS, daysOverdue } from '@karrkarr/shared';
 import { QUEUE_NAMES } from '../jobs.module';
 
 const HIGH_RISK_THRESHOLD = 70;
