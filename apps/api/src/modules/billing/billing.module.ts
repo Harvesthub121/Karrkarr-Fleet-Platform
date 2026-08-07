fe605dcf3a8b4c1b7c8ab293932f756933cc494e
Harvesthub121 <harvesthubindustries@gmail.com>
2026-08-07 21:03:40 +0800

    fix: add PolicyModule to BillingModule imports

--- apps/api/src/modules/billing/billing.module.ts
+++ apps/api/src/modules/billing/billing.module.ts
@@ -3,8 +3,10 @@ import { LedgerService } from './ledger.service';
 import { InvoiceService } from './invoice.service';
 import { InterestService } from './interest.service';
 import { BillingController } from './billing.controller';
+import { PolicyModule } from '../policy/policy.module';
 
 @Module({
+  imports: [PolicyModule],
   controllers: [BillingController],
   providers: [LedgerService, InvoiceService, InterestService],
   exports: [LedgerService, InvoiceService, InterestService],
