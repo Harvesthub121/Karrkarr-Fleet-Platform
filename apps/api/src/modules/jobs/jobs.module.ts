bb32d65a636e68436b1d7bc80dac584313ed534a
Harvesthub121 <harvesthubindustries@gmail.com>
2026-08-07 21:19:05 +0800

    fix: restore jobs.module.ts with PolicyModule import (was accidentally emptied)

---
+++ apps/api/src/modules/jobs/jobs.module.ts
@@ -0,0 +1,42 @@
+import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
+import { BillingModule } from '../billing/billing.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { PolicyModule } from '../policy/policy.module';
+import { PaymentReminderProcessor } from './processors/payment-reminder.processor';
+import { InterestAccrualProcessor } from './processors/interest-accrual.processor';
+import { ExpiryReminderProcessor } from './processors/expiry-reminder.processor';
+import { RentalStatusProcessor } from './processors/rental-status.processor';
+import { RiskScoringProcessor } from './processors/risk-scoring.processor';
+import { InvoiceGenerationProcessor } from './processors/invoice-generation.processor';
+import { JobsController } from './jobs.controller';
+import { JobsService } from './jobs.service';
+import { QUEUE_NAMES } from './jobs.constants';
+
+@Module({
+  imports: [
+    BullModule.registerQueue(
+      { name: QUEUE_NAMES.PAYMENT_REMINDER },
+      { name: QUEUE_NAMES.INTEREST_ACCRUAL },
+      { name: QUEUE_NAMES.EXPIRY_REMINDER },
+      { name: QUEUE_NAMES.RENTAL_STATUS },
+      { name: QUEUE_NAMES.RISK_SCORING },
+      { name: QUEUE_NAMES.INVOICE_GENERATION },
+    ),
+    BillingModule,
+    NotificationsModule,
+    PolicyModule,
+  ],
+  controllers: [JobsController],
+  providers: [
+    JobsService,
+    PaymentReminderProcessor,
+    InterestAccrualProcessor,
+    ExpiryReminderProcessor,
+    RentalStatusProcessor,
+    RiskScoringProcessor,
+    InvoiceGenerationProcessor,
+  ],
+  exports: [JobsService],
+})
+export class JobsModule {}
