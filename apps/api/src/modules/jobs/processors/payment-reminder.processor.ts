bd26312d05b346210832de64f01924c1c5b18894
Harvesthub121 <harvesthubindustries@gmail.com>
2026-08-08 00:04:26 +0800

    fix: typo POLCIY_KEYS -> POLICY_KEYS in payment-reminder.processor.ts

--- apps/api/src/modules/jobs/processors/payment-reminder.processor.ts
+++ apps/api/src/modules/jobs/processors/payment-reminder.processor.ts
@@ -16,7 +16,7 @@ import { Job } from 'bullmq';
 import { PrismaService } from '../../../prisma/prisma.service';
 import { PolicyService } from '../../policy/policy.service';
 import { NotificationService } from '../../notifications/notification.service';
-import { POLCIY_KEYS, parseDayLadder, formatSgd } from '@karrkarr/shared';
+import { POLICY_KEYS, parseDayLadder, formatSgd } from '@karrkarr/shared';
 import { QUEUE_NAMES } from '../jobs.constants';
 
 function sgtToday(): Date {
