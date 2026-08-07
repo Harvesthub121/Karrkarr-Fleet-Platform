22d05f0713fe0f9568d4eac88b1302d742ce3009
Harvesthub121 <harvesthubindustries@gmail.com>
2026-08-07 20:55:24 +0800

    fix: extract QUEUE_NAMES to jobs.constants to break circular dependency

--- apps/api/src/modules/jobs/processors/expiry-reminder.processor.ts
+++ apps/api/src/modules/jobs/processors/expiry-reminder.processor.ts
@@ -17,7 +17,7 @@ import { PrismaService } from '../../../prisma/prisma.service';
 import { PolicyService } from '../../policy/policy.service';
 import { NotificationService } from '../../notifications/notification.service';
 import { POLICY_KEYS, parseDayLadder } from '@karrkarr/shared';
-import { QUEUE_NAMES } from '../jobs.module';
+import { QUEUE_NAMES } from '../jobs.constants';
 
 function sgtToday(): Date {
   const now = new Date();
